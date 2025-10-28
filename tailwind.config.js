89b1f7a8a8fca10c0ecf2851d5645ac4c4f639b0
Straightouttawallstreet <joe@straightouttawallstreet.com>
2025-10-28 15:44:29 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 };
+export default config;
